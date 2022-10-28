0e26273a38a7c2327c56cc0cf6a5811775d6c85e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-10-28 13:19:46 -0400

    fix: Improve one of the comments in the util module.

--- palindromechecker/palindromechecker/util.py
+++ palindromechecker/palindromechecker/util.py
@@ -1,3 +1,3 @@
-# TODO: Add the required docstring for this module
+# TODO: Add the required docstring and any required imports of objects
 
 # TODO: implement the def get_human_readable_boolean(answer: bool) -> str function
