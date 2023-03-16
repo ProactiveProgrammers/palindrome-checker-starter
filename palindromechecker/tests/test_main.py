225c019b6ab68e2cebfcab1df02dee77d38b35bb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-03-16 10:25:48 -0400

    docs: Add more TODO reminders and advice to the test_main.py file.

--- palindromechecker/tests/test_main.py
+++ palindromechecker/tests/test_main.py
@@ -2,6 +2,14 @@
 
 # TODO: Add comments to the test cases to explain how the tests works
 
+# TODO: Review how runner.invoke works by reading the documentation
+# Reference:
+# https://typer.tiangolo.com/tutorial/testing/
+
+# TODO: Write at least three additional test cases that follow the pattern of
+# the provided test case. When creating oracles, you will need to think
+# carefully about what output should appear in the command-line interface if
+# the program contains all of the required features.
 
 def test_palindromechecker_recursive_is_palindrome():
     """Ensure that the command-line interface works for recursive approach."""
